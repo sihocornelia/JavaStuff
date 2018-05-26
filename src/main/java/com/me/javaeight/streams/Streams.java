d75b2c975f41173a1a1c652d0a5bd904fefcae12
Cornelia Sihota <siho.cornelia@gmail.com>
2018-05-26 17:14:35 +0300

    commit feature-1 branch

---
+++ src/main/java/com/me/javaeight/streams/Streams.java
@@ -0,0 +1,22 @@
+package com.me.javaeight.streams;
+
+import com.me.javaeight.collections.City;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Streams {
+
+    private static City[] arrayCities  = {
+            new City("City1"),
+            new City("City2"),
+            new City("City3")
+    };
+
+    public static void main(String[] args) {
+        List<City> empList = Arrays.asList(arrayCities);
+        empList.forEach(System.out::println);
+
+    }
+
+}
